1a96b65027eb0b15dc004cd67d0fbbb95dd1a6ea
youmikai <tourokuma4@gmail.com>
2018-11-29 20:28:32 +0900

    GoForwardUnityChan-UnityProject
    Lesson7

---
+++ Assets/CubeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+    //キューブの移動速度
+    private float speed = -0.2f;
+    //消滅位置
+    private float deadLine = -10;
+
+    public AudioClip blockSound;
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+        //画面外に出たら破棄する
+        if(transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+	}
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        string tag = c.gameObject.tag;
+        if (tag == "blockTag")
+        {
+            AudioSource.PlayClipAtPoint(blockSound, transform.position);
+        }
+    }
+}
---
+++ Assets/UIController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+    //ゲームオーバテキスト
+    private GameObject gameOvertext;
+
+    //走行距離テキスト
+    private GameObject runLengthText;
+
+    //走った距離
+    private float len = 0;
+
+    //走る速度
+    private float speed = 0.03f;
+
+    //ゲームオーバの判定
+    private bool isGameOver = false;
+
+	// Use this for initialization
+	void Start () {
+        //シーンビューからオブジェクトの実態を検索する
+        this.gameOvertext = GameObject.Find("GameOver");
+        this.runLengthText = GameObject.Find("RunLength");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.isGameOver == false)
+        {
+            //走った距離を更新する
+            this.len += this.speed;
+
+            //走った距離を表示する
+            this.runLengthText.GetComponent<Text> ().text = "Distance: " + len.ToString("F2") + "m";
+        }
+
+        //ゲームオーバになった場合
+        if(this.isGameOver == true)
+        {
+            //クリックされたらシーンをロードする
+            if (Input.GetMouseButton(0))
+            {
+                //GameSceneを読み込む
+                SceneManager.LoadScene("GameScene");
+            }
+        }
+	}
+
+    public void GameOver()
+    {
+        //ゲームオーバになったときに、画面上にゲームオーバを表示する
+        this.gameOvertext.GetComponent<Text>().text = "GameOver";
+        this.isGameOver = true;
+    }
+}
